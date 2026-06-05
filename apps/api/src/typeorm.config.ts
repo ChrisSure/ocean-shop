903fa3fce62cf02fa621f170cab001bfa789adea
tkuku <taras.kukuliak@aravo.com>
2026-06-05 16:08:43 +0300

    feat: Run main migration

--- apps/api/src/typeorm.config.ts
+++ apps/api/src/typeorm.config.ts
@@ -3,12 +3,12 @@ import { config } from 'dotenv';
 import { join } from 'path';
 
 // Load environment variables from the root .env file
-config({ path: join(__dirname, '../../../.env') });
+config({ path: join(process.cwd(), '.env') });
 
 export default new DataSource({
   type: 'postgres',
   url: process.env.DATABASE_URL,
-  entities: [join(__dirname, '**', '*.entity.{ts,js}')],
-  migrations: [join(__dirname, 'migrations', '*.{ts,js}')],
+  entities: [join(process.cwd(), 'apps/api/src/**/*.entity.{ts,js}')],
+  migrations: [join(process.cwd(), 'apps/api/src/migrations/*.{ts,js}')],
   synchronize: false,
 });
